37d970ec031e30c3fc6f225abe7989594a203ba2
Csizmadia László <csizilaci92@gmail.com>
2022-09-05 11:53:32 +0200

    Initial commit

---
+++ GentlemensClub/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GentlemensClub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
