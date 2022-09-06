d3116604f8f4aaa95c2d14bce54f26ce73c80a33
szanil <szaniszlolaszlo95@gmail.com>
2022-09-06 13:45:12 +0200

    Added an ApiHandler

---
+++ GentlemensClub/ApiHandler/ApiHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace GentlemensClub.ApiHandler;
+
+public class ApiHandler
+{
+    public async Task<T> GetDataByUrl<T>(string apiURL)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(apiURL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync(apiURL);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var table = JsonConvert.DeserializeObject<T>(data);
+                return table;
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+}
