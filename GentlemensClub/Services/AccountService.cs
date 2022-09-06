ab1625e455bd62bbaad0b4ef4c585cc25fbaa492
LGyorgy <l.gyorgy90@gmail.com>
2022-09-06 19:36:59 +0200

    Add documentation

--- GentlemensClub/Services/AccountService.cs
+++ GentlemensClub/Services/AccountService.cs
@@ -3,14 +3,27 @@ using GentlemensClub.Models.Account;
 
 namespace GentlemensClub.Services;
 
+/// <summary>
+/// A class dedicated for account related processes.
+/// </summary>
 public class AccountService
 {
+    /// <summary>
+    /// Checks if login credential contains valid username and password pair.
+    /// </summary>
+    /// <param name="credential">Login credential.</param>
+    /// <returns>Bool value for the validity of the credential.</returns>
     public bool CredentialIsValid(LoginCredential credential)
     {
         //TODO
         return credential.Username == "test" && credential.Password == "123";
     }
 
+    /// <summary>
+    /// Creates a ClaimsPrincipal from a valid LoginCredential.
+    /// </summary>
+    /// <param name="credential">Login credential.</param>
+    /// <returns>Created ClaimsPrincipal</returns>
     public ClaimsPrincipal CreateClaimsPrincipal(LoginCredential credential)
     {
         //TODO
