a48a227feafa1005e2ec484c12546df1b7032dd4
Peti <peter.hathazi99@gmail.com>
2022-09-06 12:17:26 +0200

    Create RestaurantApiController

---
+++ GentlemensClub/Controllers/ApiControllers/RestaurantApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using GentlemensClub.Models.Restaurant;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GentlemensClub.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantApiController : ControllerBase
+    {
+
+    }
+}
