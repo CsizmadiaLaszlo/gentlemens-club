0c194c4385f8ee5917f6f02689991ae8ee607f4a
Csizmadia László <csizilaci92@gmail.com>
2022-09-06 16:52:28 +0200

    Create AccountController.cs

---
+++ GentlemensClub/Controllers/Finance/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using GentlemensClub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GentlemensClub.Controllers.Finance;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public AccountController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult Account()
+    {
+        return View("~/Views/Finance/Account.cshtml");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
