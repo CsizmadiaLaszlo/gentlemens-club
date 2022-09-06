f37bfa98b63cb3bc00975250973fc3f5760c9246
Peti <peter.hathazi99@gmail.com>
2022-09-06 15:41:47 +0200

    Create TableModel

---
+++ GentlemensClub/Models/Restaurant/Table/TableModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GentlemensClub.Models.Restaurant.Table;
+
+public class TableModel
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public int SeatCount { get; set; }
+    public ReservationModel? Reservation { get; set; }
+}
