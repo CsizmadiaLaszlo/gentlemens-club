30a8f276a16ad2267f4f265aa13757e663a8879a
Csizmadia László <csizilaci92@gmail.com>
2022-09-07 16:35:53 +0200

    Create BankAccountService.cs

---
+++ GentlemensClub/Services/BankAccountService.cs
@@ -0,0 +1,20 @@
+﻿using GentlemensClub.Daos.Implementations;
+using GentlemensClub.Models.Account;
+using GentlemensClub.Models.Finance.Bank;
+
+namespace GentlemensClub.Services;
+
+public class BankAccountService
+{
+    public IBankAccountDao BankAccountDao { get; }
+    public ICurrencyDao CurrencyDao { get; }
+    public ITransactionDao TransactionDao { get; }
+
+    public BankAccountService(IBankAccountDao bankAccountDao, ICurrencyDao currencyDao, ITransactionDao transactionDao)
+    {
+        BankAccountDao = bankAccountDao;
+        CurrencyDao = currencyDao;
+        TransactionDao = transactionDao;
+    }
+
+}
