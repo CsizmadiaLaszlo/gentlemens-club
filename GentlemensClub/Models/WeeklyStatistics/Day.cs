4d0768f55ec2a3bd4b93ea350615c6eaf0d64c48
szanil <szaniszlolaszlo95@gmail.com>
2022-09-07 17:47:14 +0200

    Fixed an issue wit hthe weekly api endpoints

---
+++ GentlemensClub/Models/WeeklyStatistics/Day.cs
@@ -0,0 +1,10 @@
+﻿namespace GentlemensClub.Models.WeeklyStatistics;
+
+public class Day
+{
+    public float Open { get; set; }
+    public float High { get; set; }
+    public float Low { get; set; }
+    public float Close { get; set; }
+    public int Volume { get; set; }
+}
