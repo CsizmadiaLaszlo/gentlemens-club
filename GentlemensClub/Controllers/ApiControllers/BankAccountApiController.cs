ab86d1d594ccd9e22df376751188d6af61cebdaa
Csizmadia László <csizilaci92@gmail.com>
2022-09-07 19:37:38 +0200

    Create BankAccountApiController.cs

---
+++ GentlemensClub/Controllers/ApiControllers/BankAccountApiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using GentlemensClub.Daos.Implementations;
+using GentlemensClub.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GentlemensClub.Controllers.ApiControllers;
+
+[Route("api/finance")]
+[ApiController]
+[Authorize]
+public class BankAccountApiController : ControllerBase
+{
+    public BankAccountService BankAccountService { get; set; }
+
+    public BankAccountApiController()
+    {
+        BankAccountService = new BankAccountService(new BankAccountDao(), new CurrencyDao(), new TransactionDao());
+    }
+    
+}
