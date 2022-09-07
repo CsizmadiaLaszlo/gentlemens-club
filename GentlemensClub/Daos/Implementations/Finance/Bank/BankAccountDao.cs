66195f063d3798aaaa604f96069c20b0c0a10f9b
Csizmadia László <csizilaci92@gmail.com>
2022-09-07 16:42:45 +0200

    Create BankAccountDao.cs

---
+++ GentlemensClub/Daos/Implementations/Finance/Bank/BankAccountDao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using GentlemensClub.Models.Finance.Bank;
+namespace GentlemensClub.Daos.Implementations;
+
+public class BankAccountDao : IBankAccountDao
+{
+    public void Add(BankAccount bankAccount)
+    {
+        var idFromSqlReturn = 1;
+        bankAccount.Id = idFromSqlReturn;
+    }
+
+    public void Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BankAccount? Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BankAccount> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
