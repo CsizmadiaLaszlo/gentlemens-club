9c3820cfdf8a50668ad0494db5537153a0dbe456
Peti <peter.hathazi99@gmail.com>
2022-09-07 14:59:51 +0200

    Create RestaurantTables.js
    Not working now

---
+++ GentlemensClub/wwwroot/js/RestaurantTables.js
@@ -0,0 +1,17 @@
+﻿$("#mapsterareathing").click(function () {
+    alert("Click");
+    $("mapsterareathing").mapster({
+        singleSelect: true,
+        fill: true,
+        fillOpacity: 0.6,
+        fillColor: 'ffffff',
+        onMouseover: function (e) {
+            $(this).mapster('set', false).mapster('set', true);
+        },
+        onMouseout: function (e) {
+            $(this).mapster('set', false);
+        }
+    });
+});
+
+$("#mapsterareathing").mapster();
