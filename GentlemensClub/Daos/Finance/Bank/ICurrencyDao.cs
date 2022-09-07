0465358374c87e3c3a6e51368e38aa2729335b54
Csizmadia László <csizilaci92@gmail.com>
2022-09-07 16:41:45 +0200

    Create ICurrencyDao.cs

---
+++ GentlemensClub/Daos/Finance/Bank/ICurrencyDao.cs
@@ -0,0 +1,9 @@
+﻿using GentlemensClub.Models.Finance;
+using GentlemensClub.Models.Finance.Bank;
+
+namespace GentlemensClub.Daos.Implementations;
+
+public interface ICurrencyDao : IDao<Currency>
+{
+    List<Currency> GetAll(int bankAccountId);
+}
