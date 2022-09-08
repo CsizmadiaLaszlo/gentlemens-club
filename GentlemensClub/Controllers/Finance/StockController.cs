807188d7eee0cbc7393b9242326769e0976d6e3b
szanil <szaniszlolaszlo95@gmail.com>
2022-09-08 14:31:18 +0200

    Added apikey to configuration

--- GentlemensClub/Controllers/Finance/StockController.cs
+++ GentlemensClub/Controllers/Finance/StockController.cs
@@ -12,21 +12,25 @@ namespace GentlemensClub.Controllers.Finance;
 [Route("finance")]
 public class StockController : Controller
 {
-    const string ApiKey = "hY5rrMwpo64ZtqeqLRDztphuFoupB9N0FBffaBGU";
+    private readonly string _apiKey;
+
+    private readonly IConfiguration _configuration;
 
     private readonly ILogger<HomeController> _logger;
 
     public ApiHandlerService ApiHandler { get; set; }
 
-    public StockController(ILogger<HomeController> logger)
+    public StockController(ILogger<HomeController> logger, IConfiguration configuration)
     {
+        _configuration = configuration;
         _logger = logger;
+        _apiKey = _configuration.GetValue<string>("ApiKey");
         ApiHandler = new ApiHandlerService();
     }
 
     public async Task<int> MaxPage()
     {
-        var endpoints = (await ApiHandler.GetDataByUrl<Stocks>($"https://api.stockdata.org/v1/entity/search?exchanges=NASDAQ&api_token={ApiKey}")).Meta;
+        var endpoints = (await ApiHandler.GetDataByUrl<Stocks>($"https://api.stockdata.org/v1/entity/search?exchanges=NASDAQ&api_token={_apiKey}")).Meta;
         var maxPage = endpoints.Found / endpoints.Limit + 1;
         return maxPage;
     }
@@ -35,7 +39,7 @@ public class StockController : Controller
     public async Task<IActionResult> Stock(int page = 1)
     {
         var stocks =
-            await ApiHandler.GetDataByUrl<Stocks>($"https://api.stockdata.org/v1/entity/search?exchanges=NASDAQ&page={page}&api_token={ApiKey}");
+            await ApiHandler.GetDataByUrl<Stocks>($"https://api.stockdata.org/v1/entity/search?exchanges=NASDAQ&page={page}&api_token={_apiKey}");
         var stockList = stocks.Data;
         ViewBag.StockList = stockList;
         ViewBag.Page = page;
@@ -48,7 +52,7 @@ public class StockController : Controller
     {
         var stockInfo =
             (await ApiHandler.GetDataByUrl<TodayStatistic>(
-                $"https://api.stockdata.org/v1/data/quote?symbols={symbol}&api_token={ApiKey}")).Data;
+                $"https://api.stockdata.org/v1/data/quote?symbols={symbol}&api_token={_apiKey}")).Data;
         var todayInfo = stockInfo.Select(x => x);
         ViewBag.TodayInfo = todayInfo;
         return View("~/Views/Finance/StockInformation/SelectedStock.cshtml");
@@ -59,7 +63,7 @@ public class StockController : Controller
     {
         var stockInfo =
             (await ApiHandler.GetDataByUrl<WeeklyStatistics>(
-                $"https://api.stockdata.org/v1/data/intraday?symbols={symbol}&api_token={ApiKey}")).Data;
+                $"https://api.stockdata.org/v1/data/intraday?symbols={symbol}&api_token={_apiKey}")).Data;
         var weeklyInfo = stockInfo.Select(x => x);
         ViewBag.WeeklyInfo = weeklyInfo;
         return View("~/Views/Finance/StockInformation/WeeklyStatistics.cshtml");
@@ -70,7 +74,7 @@ public class StockController : Controller
     {
         var stockInfo =
             (await ApiHandler.GetDataByUrl<YearlyStatistics>(
-                $"https://api.stockdata.org/v1/data/eod?symbols={symbol}&api_token={ApiKey}")).Data;
+                $"https://api.stockdata.org/v1/data/eod?symbols={symbol}&api_token={_apiKey}")).Data;
         var yearlyInfo = stockInfo.Select(x => x);
         ViewBag.YearlyInfo = yearlyInfo;
         return View("~/Views/Finance/StockInformation/YearlyStatistics.cshtml");
