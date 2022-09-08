da0c8f8eb6870076bd1e021e748aa95b7d4a457d
Csizmadia László <csizilaci92@gmail.com>
2022-09-08 09:40:21 +0200

    Update GetAllCurrency

--- GentlemensClub/wwwroot/js/Finance/Bank/BankAccount.js
+++ GentlemensClub/wwwroot/js/Finance/Bank/BankAccount.js
@@ -17,24 +17,23 @@ async function GetCurrency(acronym) {
     return await fetch(url).then(r => r.json());
 }
 
-async function GetAllCurrency(){
+async function GetAllCurrency() {
     const url = `../api/finance/currencies`;
     return await fetch(url).then(r => r.json());
 }
 
 async function LoadCurrency(acronym) {
     let currency = await GetCurrency(acronym);
-    
+
     document.querySelector('.currency-value').innerHTML = currency["Value"];
     document.querySelector('.currency-symbol').innerHTML = currency["Symbol"];
     document.querySelector('.currency-name').innerHTML = currency["Name"];
     let flagElement = document.querySelector('.flag-icon');
     let currentCountry = flagElement.dataset.country;
-    console.log(currentCountry)
     flagElement.classList.remove(`fi-${currentCountry}`);
     flagElement.classList.add(`fi-${currency["Country"]}`);
     flagElement.dataset.country = currency["Country"];
-    
+
     SetDropDownElements(acronym);
 }
 
