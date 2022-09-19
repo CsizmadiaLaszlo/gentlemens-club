c48463fff24edec31a32d150138f72e7c038d599
Bárdos István <bardos.istvan@icloud.com>
2022-09-19 16:45:02 +0200

    Add List of Currencies and TransactionModels to BankAccount

--- GentlemensClub/Models/Finance/Bank/BankAccount.cs
+++ GentlemensClub/Models/Finance/Bank/BankAccount.cs
@@ -7,4 +7,6 @@ public class BankAccount
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public int AccountId { get; set; }
+    public List<Currency>? Currencies { get; set; }
+    public List<TransactionModel>? TransactionModels { get; set; }
 }
