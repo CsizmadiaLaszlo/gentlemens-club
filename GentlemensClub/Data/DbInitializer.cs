23b1e811dd3d5ee573260618d926af256d0d9789
Bárdos István <bardos.istvan@icloud.com>
2022-09-19 16:51:10 +0200

    Create DB with "code first"

--- GentlemensClub/Data/DbInitializer.cs
+++ GentlemensClub/Data/DbInitializer.cs
@@ -4,4 +4,13 @@ namespace GentlemensClub.Data;
 
 public class DbInitializer
 {
+    public static void Initialize(GentlemensClubContext context)
+    {
+        context.Database.EnsureCreated();
+
+        if (context.Accounts.Any())
+        {
+            return;
+        }
+    }
 }
