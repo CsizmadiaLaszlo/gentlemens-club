8f4131963a59aba1931d5e81f0539aae950cba5f
Bárdos István <bardos.istvan@icloud.com>
2022-09-19 16:54:47 +0200

    Update Program.cs to Initialize() SQL DB with EF Core

--- GentlemensClub/Program.cs
+++ GentlemensClub/Program.cs
@@ -1,3 +1,6 @@
+using GentlemensClub.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,11 @@ builder.Services.AddAuthentication("LoginCookieAuth").AddCookie("LoginCookieAuth
     options.Cookie.Name = "LoginCookieAuth";
 });
 
+builder.Services.AddDbContext<GentlemensClubContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("GentlemensClubConnectionString") ??
+                         throw new InvalidOperationException("Connection string not found!")));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +24,15 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<GentlemensClubContext>();
+
+    DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
