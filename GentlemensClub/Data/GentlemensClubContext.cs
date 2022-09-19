0c3550926ba11736b867843bc1dca12a15b7e5ea
Bárdos István <bardos.istvan@icloud.com>
2022-09-19 16:45:23 +0200

    Create GentlemensClubContext.cs

---
+++ GentlemensClub/Data/GentlemensClubContext.cs
@@ -0,0 +1,9 @@
+using GentlemensClub.Models.Account;
+using GentlemensClub.Models.Finance.Bank;
+using Microsoft.EntityFrameworkCore;
+
+namespace GentlemensClub.Data;
+
+public class GentlemensClubContext : DbContext
+{
+}
