4d8999daacefba98606f11e940693379842dd8d2
Bárdos István <bardos.istvan@icloud.com>
2022-09-20 17:18:30 +0200

    Define methods for DB manipulation at IAccountService

--- GentlemensClub/Services/Interfaces/IAccountService.cs
+++ GentlemensClub/Services/Interfaces/IAccountService.cs
@@ -5,4 +5,8 @@ namespace GentlemensClub.Services.Interfaces;
 
 public interface IAccountService
 {
+    Task<bool> CredentialIsValid(LoginCredential credential);
+    Task<ClaimsPrincipal> CreateClaimsPrincipal(LoginCredential credential);
+    Task CreateAccount(RegistrationData data);
+    Task<bool> RegistrationIsValid(RegistrationData data);
 }
