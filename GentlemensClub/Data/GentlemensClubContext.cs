aaa7e090cecbc8538b66db6438de5b2d209eb1e4
Csizmadia László <csizilaci92@gmail.com>
2022-09-20 09:22:00 +0200

    Update GentlemensClubContext.cs

--- GentlemensClub/Data/GentlemensClubContext.cs
+++ GentlemensClub/Data/GentlemensClubContext.cs
@@ -8,6 +8,8 @@ public class GentlemensClubContext : DbContext
 {
     public DbSet<Account> Accounts { get; set; }
     public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<BankCurrency> BankCurrencies { get; set; }
+    public DbSet<BankTransaction> BankTransactions { get; set; }
 
     public GentlemensClubContext(DbContextOptions<GentlemensClubContext> options) : base(options)
     {
@@ -16,7 +18,12 @@ public class GentlemensClubContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // User account
         modelBuilder.Entity<Account>().ToTable("account");
+        // User bank account
         modelBuilder.Entity<BankAccount>().ToTable("bank_account");
+        modelBuilder.Entity<BankCurrency>().ToTable("bank_currency");
+        modelBuilder.Entity<BankTransaction>().ToTable("bank_transaction");
+        // stb
     }
 }
