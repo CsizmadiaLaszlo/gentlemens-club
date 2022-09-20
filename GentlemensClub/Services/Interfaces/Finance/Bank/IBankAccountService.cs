7b1070c700a7288f8d8b8bc3f1652eb00846d5ba
Csizmadia László <csizilaci92@gmail.com>
2022-09-20 09:25:03 +0200

    Create IBankAccountService.cs

---
+++ GentlemensClub/Services/Interfaces/Finance/Bank/IBankAccountService.cs
@@ -0,0 +1,12 @@
+﻿using GentlemensClub.Models.Account;
+using GentlemensClub.Models.Finance.Bank;
+
+namespace GentlemensClub.Services.Interfaces.Finance.Bank;
+
+public interface IBankAccountService
+{
+    Task AddBankAccount(Account account);
+    Task<BankAccount> GetBankAccount(int id);
+    Task DeleteBankAccount(int id);
+    Task UpdateBankAccount(BankAccount bankAccount);
+}
