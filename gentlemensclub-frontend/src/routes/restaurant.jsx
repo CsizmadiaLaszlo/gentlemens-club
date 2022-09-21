176ef14c71d82dae3a53eec411e530cbe89a1b2a
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 12:16:41 +0200

    Add base container for routes

--- gentlemensclub-frontend/src/routes/restaurant.jsx
+++ gentlemensclub-frontend/src/routes/restaurant.jsx
@@ -4,7 +4,11 @@ export default function Restaurant() {
     return (
         <div>
             <Header></Header>
-                <h1>restaurant</h1>
+            <div className={"container"}>
+                <main role={"main"} className={"pb-3"}>
+                    <h1>restaurant</h1>
+                </main>
+            </div>
             <Footer></Footer>
         </div>
     );
