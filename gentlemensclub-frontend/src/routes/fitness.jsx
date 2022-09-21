e481db019f1e096f7f83b9ca41de0f675ed98bbc
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 11:48:45 +0200

    Create fitness.jsx

---
+++ gentlemensclub-frontend/src/routes/fitness.jsx
@@ -0,0 +1,11 @@
+import {Footer, Header} from "../components/layout";
+
+export default function Fitness() {
+    return (
+        <div>
+            <Header></Header>
+            <h1>fitness</h1>
+            <Footer></Footer>
+        </div>
+    );
+}
