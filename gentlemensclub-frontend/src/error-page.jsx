7e70562c47e8cbe1e995918bd8d65f4862d04d60
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 14:22:05 +0200

    Update error-page.jsx

--- gentlemensclub-frontend/src/error-page.jsx
+++ gentlemensclub-frontend/src/error-page.jsx
@@ -1,18 +1,26 @@
-import {useRouteError} from "react-router-dom";
+import {useRouteError, useNavigate} from "react-router-dom";
 
 export default function ErrorPage() {
     const error = useRouteError();
+    const navigate = useNavigate();
     console.error(error);
 
     return (
         <div className={"container"}>
             <main role={"main"} className={"pb-3"}>
-                <div id="error-page">
-                    <h1>Oops!</h1>
-                    <p>Sorry, an unexpected error has occurred.</p>
-                    <p>
-                        <i>{error.statusText || error.message}</i>
-                    </p>
+                <div className={"text-center"}>
+                    <div id="error-page">
+                        <h1>Oops!</h1>
+                        <p>Sorry, an unexpected error has occurred.</p>
+                        <p>
+                            <i>{error.statusText || error.message}</i>
+                        </p>
+                        <button className={"btn btn-outline-secondary"} type="button" onClick={() => {
+                            navigate(-1);
+                        }}>
+                            Back
+                        </button>
+                    </div>
                 </div>
             </main>
         </div>
