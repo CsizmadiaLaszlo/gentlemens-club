9f4355a744c0e6459b4cb7c496d1cda9ad16cb35
Bárdos István <bardos.istvan@icloud.com>
2022-09-21 21:20:52 +0200

    Create CreateTablesList() method in DbInitializer.cs
    Created method will be used during initial database record creation for Restaurant type.

--- GentlemensClub/Data/DbInitializer.cs
+++ GentlemensClub/Data/DbInitializer.cs
@@ -86,5 +86,29 @@ public class DbInitializer
         };
         context.BankAccounts.Add(bankAccount);
         context.SaveChanges();
+    private static List<Table> CreateTablesList()
+    {
+        List<Table> tables = new();
+        
+        const string chars = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ";
+        Random random = new Random(6969);
+        for (int i = 0; i < 25; i++)
+        {
+            Table newTable = new Table();
+            Reservation newReservation = new Reservation();
+            newTable.Id = i + 1;
+            newTable.Description = new string(Enumerable.Repeat(chars, 50)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            newTable.SeatCount = random.Next(2, 6);
+            if (random.Next(0, 100) > 50)
+            {
+                newReservation.Id = random.Next(0, 100);
+                newReservation.ReservationStartDate = DateTime.Now;
+                newTable.Reservation = newReservation;
+            }
+            tables.Add(newTable);
+        }
+
+        return tables;
     }
 }
