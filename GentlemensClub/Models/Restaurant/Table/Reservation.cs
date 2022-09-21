44c19a17674eb4368f552d1318b722ac69b8af81
Bárdos István <bardos.istvan@icloud.com>
2022-09-21 16:20:32 +0200

    Change Id property at Reservation model to be DB generated

--- GentlemensClub/Models/Restaurant/Table/Reservation.cs
+++ GentlemensClub/Models/Restaurant/Table/Reservation.cs
@@ -1,7 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GentlemensClub.Models.Restaurant.Table;
 
 public class Reservation
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public DateTime ReservationStartDate { get; set; }
     public int Member { get; set; }
