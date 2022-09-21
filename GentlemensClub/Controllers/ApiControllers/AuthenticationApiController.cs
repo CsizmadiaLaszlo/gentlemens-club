676f64cb165c33ed149608a6f4eeedb7a9c591e4
LGyorgy <l.gyorgy90@gmail.com>
2022-09-21 10:34:52 +0200

    Create AuthenticationApiController

---
+++ GentlemensClub/Controllers/ApiControllers/AuthenticationApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GentlemensClub.Controllers.ApiControllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationApiController : ControllerBase
+    {
+    }
+}
