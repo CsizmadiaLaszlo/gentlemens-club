36cc365b6c44deeedfbcf1bbcdcfb506c1a17901
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 11:48:38 +0200

    Create bank.jsx

---
+++ gentlemensclub-frontend/src/routes/bank.jsx
@@ -0,0 +1,15 @@
+﻿import {Footer, Header} from "../components/layout";
+
+export default function Bank() {
+    return (
+        <div>
+            <Header></Header>
+            <div className={"container"}>
+                <main role={"main"} className={"pb-3"}>
+                    <h1>bank</h1>
+                </main>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+}
