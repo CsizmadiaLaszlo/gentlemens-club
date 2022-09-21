bec78025410eec804b96d8bc8f06b3b7d3567c3a
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 12:27:03 +0200

    Create finance.jsx
    Rename bank to finance

---
+++ gentlemensclub-frontend/src/routes/finance.jsx
@@ -0,0 +1,15 @@
+﻿import {Footer, Header} from "../components/layout";
+
+export default function Finance() {
+    return (
+        <div>
+            <Header></Header>
+            <div className={"container"}>
+                <main role={"main"} className={"pb-3"}>
+                    <h1>finance</h1>
+                </main>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+}
