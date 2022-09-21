727243c4febef4fea8fac5ebe895ab2d926e1903
Bárdos István <bardos.istvan@icloud.com>
2022-09-21 16:20:24 +0200

    Change Id property at Table model to be DB generated

--- GentlemensClub/Models/Restaurant/Table/Table.cs
+++ GentlemensClub/Models/Restaurant/Table/Table.cs
@@ -1,7 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GentlemensClub.Models.Restaurant.Table;
 
 public class Table
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Description { get; set; }
     public int SeatCount { get; set; }
