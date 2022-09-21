3fcf6bdeacd629c5ccb1302966a05438dda5223b
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 11:48:51 +0200

    Create membership.jsx

---
+++ gentlemensclub-frontend/src/routes/membership.jsx
@@ -0,0 +1,11 @@
+﻿import {Footer, Header} from "../components/layout";
+
+export default function Membership() {
+    return (
+        <div>
+            <Header></Header>
+            <h1>membership</h1>
+            <Footer></Footer>
+        </div>
+    );
+}
