b1866090c11e92774ab3b73c3472f93c9c2b92d2
szanil <szaniszlolaszlo95@gmail.com>
2022-09-21 10:24:34 +0200

    Create IStockApiService.cs

---
+++ GentlemensClub/Services/Interfaces/Finance/Stock/IStockApiService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace GentlemensClub.Services.Interfaces.Finance.Stock;
+
+public interface IStockApiService
+{
+    Task<IEnumerable> Stock(int? page = 1);
+    Task<IEnumerable> StockInfo(string? symbol);
+    Task<IEnumerable> WeeklyStatistics(string? symbol);
+    Task<IEnumerable> YearlyStatistics(string? symbol);
+    Task<int> MaxPage();
+}
