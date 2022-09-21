c8726ea6ba374bd9d334498144d9a96ab86c5029
Csizmadia László <csizilaci92@gmail.com>
2022-09-21 17:39:28 +0200

    Update bankApiHandler.js

--- gentlemensclub-frontend/src/js/finance/bank/bankApiHandler.js
+++ gentlemensclub-frontend/src/js/finance/bank/bankApiHandler.js
@@ -1,12 +1,12 @@
 export async function getCurrency(acronym) {
-    const url = `api/finance/bank/currency?acronym=${acronym}`;
+    const url = `/api/finance/bank/currency?acronym=${acronym}`;
     return await fetch(url).then(r => r.json());
 }
 
 export async function getAllCurrency() {
-    return await fetch("api/finance/bank/currencies").then(r => r.json());
+    return await fetch("/api/finance/bank/currencies").then(r => r.json());
 }
 
 export async function getAllTransaction() {
-    return await fetch("api/finance/bank/transaction").then(r => r.json());
+    return await fetch("/api/finance/bank/transaction").then(r => r.json());
 }
