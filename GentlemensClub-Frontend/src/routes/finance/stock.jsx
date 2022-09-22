560d38fc8b3d91d2fb2f5c2c71dd5feab5b5073b
szanil <szaniszlolaszlo95@gmail.com>
2022-09-22 15:03:56 +0200

    Create stock.jsx

---
+++ GentlemensClub-Frontend/src/routes/finance/stock.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+
+import { getStocks, getMaxPage } from "../../js/finance/stock/stockApiHandler";
+
+
+export default class Stock extends Component {
+}
