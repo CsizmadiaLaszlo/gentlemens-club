5d765c9111ded2b0dec7509c11df7439b6d80a5c
Peti <peter.hathazi99@gmail.com>
2022-09-22 19:31:11 +0200

    Update restaurantApiHandler.js

--- GentlemensClub-Frontend/src/js/restaurant/restaurantApiHandler.js
+++ GentlemensClub-Frontend/src/js/restaurant/restaurantApiHandler.js
@@ -2,3 +2,8 @@ export async function getTableReservations() {
     var apiUrl = '/api/RestaurantApi/get-table-reservations';
     return await fetch(apiUrl).then((response) => response.json());
 }
+
+export async function loadTableData(selectedTableId) {
+    var apiUrl = `/api/RestaurantApi/get-table-data?tableId=${selectedTableId}`;
+    return await fetch(apiUrl).then((response) => response.json());
+}
