5ff1ba7f296d5defef5b904ba2c29ded2804d04b
szanil <szaniszlolaszlo95@gmail.com>
2022-09-22 15:04:17 +0200

    Update stock.jsx
    Added class constructor

--- GentlemensClub-Frontend/src/routes/finance/stock.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock.jsx
@@ -5,4 +5,18 @@ import { getStocks, getMaxPage } from "../../js/finance/stock/stockApiHandler";
 
 
 export default class Stock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stocks: [],
+            page: 1,
+            next: 2,
+            prev: 0,
+            maxPage: 0,
+            loading: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
 }
