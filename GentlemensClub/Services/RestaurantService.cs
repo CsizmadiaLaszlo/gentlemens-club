8f06eef77cac733660df45087a1531a92f505067
Bárdos István <bardos.istvan@icloud.com>
2022-09-22 17:24:29 +0200

    Create RestaurantService.cs

---
+++ GentlemensClub/Services/RestaurantService.cs
@@ -0,0 +1,10 @@
+using GentlemensClub.Data;
+using GentlemensClub.Models.Restaurant.Table;
+using GentlemensClub.Services.Interfaces.Restaurant.Table;
+using Microsoft.EntityFrameworkCore;
+
+namespace GentlemensClub.Services;
+
+public class RestaurantService : IRestaurantService
+{
+}
