ea557d9ee1561ff7eba89d7d5e84e99519271680
Bárdos István <bardos.istvan@icloud.com>
2022-09-22 17:21:48 +0200

    Define basic CRUD operations without implementations
    All CRUD operation will work with SQL DB through Entity Framework, therefore each of them should be asynchronous.

--- GentlemensClub/Services/Interfaces/Restaurant/Table/IRestaurantService.cs
+++ GentlemensClub/Services/Interfaces/Restaurant/Table/IRestaurantService.cs
@@ -4,4 +4,10 @@ namespace GentlemensClub.Services.Interfaces.Restaurant.Table;
 
 public interface IRestaurantService : IReservationService
 {
+    Task Add(RestaurantTable table);
+    Task Remove(int id);
+    Task<RestaurantTable?> Get(int id);
+    Task<IEnumerable<RestaurantTable>> GetAll();
+    
+    Task<Dictionary<int, Reservation?>> GetTableReservations();
 }
