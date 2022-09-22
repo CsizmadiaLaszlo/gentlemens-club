6fb2d4553db9f9ebb7efce8b9273b1e18adc8bfd
szanil <szaniszlolaszlo95@gmail.com>
2022-09-22 19:01:59 +0200

    Update selectedStock.jsx
    Added constructor

--- GentlemensClub-Frontend/src/routes/finance/selectedStock.jsx
+++ GentlemensClub-Frontend/src/routes/finance/selectedStock.jsx
@@ -5,4 +5,14 @@ import React, { Component } from 'react';
 import { getSelectedStock } from "../../js/finance/stock/stockApiHandler";
 
 export default class SelectedStock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stock: [],
+            symbol: "",
+            loading: true
+        };
+    }
+
 }
