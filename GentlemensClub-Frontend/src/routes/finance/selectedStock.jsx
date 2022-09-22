3d7bf5ee6f10d5ef429275670ef2b75e6581dde5
szanil <szaniszlolaszlo95@gmail.com>
2022-09-22 19:01:46 +0200

    Create selectedStock.jsx

---
+++ GentlemensClub-Frontend/src/routes/finance/selectedStock.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+
+import React, { Component } from 'react';
+
+import { getSelectedStock } from "../../js/finance/stock/stockApiHandler";
+
+export default class SelectedStock extends Component {
+}
