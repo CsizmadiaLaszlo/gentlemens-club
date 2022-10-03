aee0c015b8642c7646b5d867077be7dacaf041db
LGyorgy <l.gyorgy90@gmail.com>
2022-10-03 15:37:09 +0200

    Refactor registration modal into function component

--- GentlemensClub-Frontend/src/js/authentication/authenticationUtils.js
+++ GentlemensClub-Frontend/src/js/authentication/authenticationUtils.js
@@ -18,3 +18,27 @@ export async function requestJwtToken(username, password) {
 
     return response;
 }
+
+
+export async function requestAccountRegistration(username, email, password, confirmPassword) {
+    const url = "api/authentication/register";
+
+    const credentials = {
+        "username": username,
+        "email": email,
+        "password": password,
+        "confirmPassword": confirmPassword
+    };
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials),
+    };
+
+    const response = await fetch(url, options);
+
+    return response;
+}
