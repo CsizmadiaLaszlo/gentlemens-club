d0b05e521649f0dffe9551647f7b0560f20032e3
Bárdos István <bardos.istvan@icloud.com>
2022-10-03 15:11:23 +0200

    Create ContactApiController.cs

---
+++ GentlemensClub-Backend/Controllers/ApiControllers/ContactApiController.cs
@@ -0,0 +1,10 @@
+using GentlemensClub.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GentlemensClub.Controllers.ApiControllers;
+
+[Route("api/contact")]
+[ApiController]
+public class ContactApiController : ControllerBase
+{
+}
