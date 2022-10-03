94926b33a420353438f7c6ea14dec8feb6e9f43f
LGyorgy <l.gyorgy90@gmail.com>
2022-10-03 15:37:09 +0200

    Refactor registration modal into function component

--- GentlemensClub-Frontend/src/components/authentication/registrationModal.jsx
+++ GentlemensClub-Frontend/src/components/authentication/registrationModal.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState} from "react";
 import { Modal } from "../shared/modal.jsx";
+import { requestAccountRegistration } from "../../js/authentication/authenticationUtils.js";
 
 
 const RegistrationModal = ({ show, onSuccess, onClose }) => {
@@ -12,77 +13,48 @@ const RegistrationModal = ({ show, onSuccess, onClose }) => {
     );
 }
 
-class RegistrationForm extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            username: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-        }
+const RegistrationForm = ({ onSuccess }) => {
+    const [error, setError] = useState(null);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    async requestAccountRegistration(username, email, password, confirmPassword) {
-        const url = "api/authentication/register";
-
-        const credentials = {
-            "username": username,
-            "email": email,
-            "password": password,
-            "confirmPassword": confirmPassword
+    const handleInputChange = (e) => {
+        const setters = {
+            error: setError,
+            username: setUsername,
+            email: setEmail,
+            password: setPassword,
+            confirmPassword: setConfirmPassword,
         };
 
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials),
-        };
+        const name = e.target.name;
+        const value = e.target.value;
 
-        const response = await fetch(url, options);
+        setters[name](value);
+    };
 
-        return response;
-    }
-
-    async handleSubmit(event) {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({ error: null });
-        const username = this.state.username;
-        const password = this.state.password;
-        const email = this.state.email;
-        const confirmPassword = this.state.confirmPassword;
+        setError(null);
 
         if (password !== confirmPassword) {
-            const error = "Password confirmation doesn't match Password."
-            this.setState({ error });
+            setError("Password confirmation doesn't match Password.");
             return;
         }
 
-        const response = await this.requestAccountRegistration(username, email, password, confirmPassword);
+        const response = await requestAccountRegistration(username, email, password, confirmPassword);
         if (response.status === 409) {
-            this.setState({ error: "This username or e-mail address is already taken." });
+            setError("This username or e-mail address is already taken.");
             return;
         }
         else if (response.status !== 200) {
-            this.setState({ error: "Something went wrong..." });
+            setError("Something went wrong...");
             return;
         }
+
         const data = await response.json();
         const token = data.access_token;
         const expiresAt = data.expires_at;
@@ -90,38 +62,37 @@ class RegistrationForm extends React.Component {
         localStorage.setItem("jwt", token);
         localStorage.setItem("jwtExpiresAt", new Date(expiresAt).toUTCString());
 
-        this.props.onSuccess();
-    }
+        onSuccess();
+    };
 
-    render() {
-        return (
-            <form onSubmit={async (event) => { await this.handleSubmit(event) }}>
-                <div className='text-danger'>{this.state.error !== null && this.state.error}</div>
-                <div className={"mb-3"}>
-                    <label className={"form-label"}>Username:</label>
-                    <input className={"form-control"} onChange={this.handleInputChange} type="text" name="username" />
+    return (
+        <form onSubmit={async (event) => { await handleSubmit(event) }}>
+            <div className='text-danger'>{error !== null && error}</div>
+            <div className={"mb-3"}>
+                <label className={"form-label"}>Username:</label>
+                <input className={"form-control"} onChange={handleInputChange} type="text" name="username" />
 
-                </div>
-                <div className={"mb-3"}>
-                    <label className={"form-label"}>E-mail address:</label>
-                    <input className={"form-control"} onChange={this.handleInputChange} type="email" name="email" />
+            </div>
+            <div className={"mb-3"}>
+                <label className={"form-label"}>E-mail address:</label>
+                <input className={"form-control"} onChange={handleInputChange} type="email" name="email" />
 
-                </div>
-                <div className={"mb-3"}>
-                    <label className={"form-label"}>Password:</label>
-                    <input className={"form-control"} onChange={this.handleInputChange} type="password" name="password" />
+            </div>
+            <div className={"mb-3"}>
+                <label className={"form-label"}>Password:</label>
+                <input className={"form-control"} onChange={handleInputChange} type="password" name="password" />
 
-                </div>
-                <div className={"mb-3"}>
-                    <label className={"form-label"}>Confirm Password:</label>
-                    <input className={"form-control"} onChange={this.handleInputChange} type="password" name="confirmPassword" />
-                </div>
-                <div>
-                    <input type="submit" className={"btn btn-dark"} value="Register" />
-                </div>
-            </form>
-        );
-    }
+            </div>
+            <div className={"mb-3"}>
+                <label className={"form-label"}>Confirm Password:</label>
+                <input className={"form-control"} onChange={handleInputChange} type="password" name="confirmPassword" />
+            </div>
+            <div>
+                <input type="submit" className={"btn btn-dark"} value="Register" />
+            </div>
+        </form>
+    );
 }
+
 
 export default RegistrationModal;
