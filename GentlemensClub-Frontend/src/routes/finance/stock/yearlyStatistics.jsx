f0c735e0ec8c0ee4027ed87ef326b20d929c90a2
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:58:29 +0200

    Update yearlyStatistics.jsx
    Using useState hooks

--- GentlemensClub-Frontend/src/routes/finance/stock/yearlyStatistics.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock/yearlyStatistics.jsx
@@ -4,6 +4,11 @@ import { LoadingSpinner } from "../../../components/shared";
 import { getYearlyStock } from "../../../js/finance/stock/stockApiHandler";
 
 const YearlyStatistics = () => {
+
+    const [yearlyStatistic, setYearlyStatistic] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { symbol } = useParams();
+
 }
 
 export default YearlyStatistics;
