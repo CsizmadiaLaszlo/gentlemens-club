9cb11917bb4294d2407666ca6edb48c180cb1747
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:49:09 +0200

    Replaced stock in a separate folder and renamed it to stocks

---
+++ GentlemensClub-Frontend/src/routes/finance/stock/stocks.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect, createContext, useContext } from 'react';
+import { LoadingSpinner } from "../../../components/shared";
+import { getStocks, getMaxPage } from "../../../js/finance/stock/stockApiHandler";
+
+const Stock = () => {
+}
+
+export default Stock;
