08dfda4396d65f1c49f1fb5ecc312887cfff7c91
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:50:37 +0200

    Update stocks.jsx
    Using useState hooks

--- GentlemensClub-Frontend/src/routes/finance/stock/stocks.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock/stocks.jsx
@@ -4,6 +4,27 @@ import { LoadingSpinner } from "../../../components/shared";
 import { getStocks, getMaxPage } from "../../../js/finance/stock/stockApiHandler";
 
 const Stock = () => {
+
+    const [stocks, setStocks] = useState([]);
+    const [page, setPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+    const stocksLoader = async () => {
+        const stocks = await getStocks(page);
+        const maxPage = await getMaxPage();
+        if (!stocks || !maxPage) {
+            throw new Response("",
+                {
+                    status: 404,
+                    statusText: "Not Found",
+                });
+        }
+        setStocks(stocks);
+        setMaxPage(maxPage.maxPage);
+        setLoading(false);
+    }
+
 }
 
 export default Stock;
