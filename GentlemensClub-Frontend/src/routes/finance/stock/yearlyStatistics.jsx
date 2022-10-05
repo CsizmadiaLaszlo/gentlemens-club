05613efff1594c449f70883613251b31c545fc3a
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:58:07 +0200

    Create yearlyStatistics.jsx

---
+++ GentlemensClub-Frontend/src/routes/finance/stock/yearlyStatistics.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, createContext, useContext } from 'react';
+import { useParams } from "react-router-dom";
+import { LoadingSpinner } from "../../../components/shared";
+import { getYearlyStock } from "../../../js/finance/stock/stockApiHandler";
+
+const YearlyStatistics = () => {
+}
+
+export default YearlyStatistics;
