3eb3543f2f021b42ff7ac56ec09907fea185aaae
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:59:45 +0200

    Update yearlyStatistics.jsx
    Added renderStatistics function to generate the view of statistics in cards

--- GentlemensClub-Frontend/src/routes/finance/stock/yearlyStatistics.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock/yearlyStatistics.jsx
@@ -26,6 +26,35 @@ const YearlyStatistics = () => {
         yearlyStatisticLoader();
     }, []);
 
+
+    const renderStatistics = () => {
+        return (
+            <div>
+                <div>
+                    {Object.keys(yearlyStatistic).length === 0
+                        ? <div className="text-white d-flex flex-wrap justify-content-center">
+                            <h2>No available data!</h2></div>
+                        : <div className="text-white d-flex flex-wrap justify-content-center">
+                            <h1>Past week statistics</h1></div>
+                    }
+                </div>
+                <div className="d-flex flex-wrap">
+                    {yearlyStatistic.map(st => {
+                        return (
+                            <div className="stock card bg-dark text-white border-light">
+                                <div>{st.date}</div>
+                                <div>High: {st.high}</div>
+                                <div>Low: {st.low}</div>
+                                <div>Open: {st.open}</div>
+                                <div>Close: {st.close}</div>
+                                <div>Volume: {st.volume}</div>
+                            </div>);
+                    })}
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default YearlyStatistics;
