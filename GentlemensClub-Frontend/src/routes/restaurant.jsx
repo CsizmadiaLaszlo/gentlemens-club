d2960bd9be006b5d3d9196b588a23538c1e235c8
Peti <peter.hathazi99@gmail.com>
2022-10-05 21:43:00 +0200

    Update restaurant.jsx

--- GentlemensClub-Frontend/src/routes/restaurant.jsx
+++ GentlemensClub-Frontend/src/routes/restaurant.jsx
@@ -1,14 +1,44 @@
 import { Footer } from '../components/layout';
 import { RestaurantHeader } from '../components/restaurant/restaurantLayoutComponents';
-import RestaurantApp from '../routes/restaurant/table';
+import RestaurantHomeApp from './restaurant/home';
+import RestaurantMenuApp from './restaurant/menu';
+import RestaurantTableApp from './restaurant/table';
 
-export default function RestaurantHome() {
+export function RestaurantHome() {
     return (
         <div>
             <RestaurantHeader />
             <div className={"container"}>
                 <main role={"main"}>
-                    <RestaurantApp />
+                    <RestaurantHomeApp />
+                </main>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export function RestaurantMenu() {
+    return (
+        <div>
+            <RestaurantHeader />
+            <div className={"container"}>
+                <main role={"main"}>
+                    <RestaurantMenuApp />
+                </main>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export function RestaurantTable() {
+    return (
+        <div>
+            <RestaurantHeader />
+            <div className={"container"}>
+                <main role={"main"}>
+                    <RestaurantTableApp />
                 </main>
             </div>
             <Footer />
