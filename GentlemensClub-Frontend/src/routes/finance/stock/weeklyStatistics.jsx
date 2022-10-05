8e5097d96e5d888d4f59c3b7e17f13cd855174a3
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:55:54 +0200

    Update weeklyStatistics.jsx
    Using useState hooks

--- GentlemensClub-Frontend/src/routes/finance/stock/weeklyStatistics.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock/weeklyStatistics.jsx
@@ -4,6 +4,11 @@ import { LoadingSpinner } from "../../../components/shared";
 import { getWeeklyStock } from "../../../js/finance/stock/stockApiHandler";
 
 const WeeklyStatistics = () => {
+
+    const [weeklyStatistic, setWeeklyStatistic] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { symbol } = useParams();
+
 }
 
 export default WeeklyStatistics;
