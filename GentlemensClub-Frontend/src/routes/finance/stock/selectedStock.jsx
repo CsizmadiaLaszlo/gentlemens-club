716a659aa66e9d30c77eb07b938d17a1bbe923b5
szanil <szaniszlolaszlo95@gmail.com>
2022-10-05 09:40:23 +0200

    Update selectedStock.jsx
    Replaced class component to function in react

--- GentlemensClub-Frontend/src/routes/finance/stock/selectedStock.jsx
+++ GentlemensClub-Frontend/src/routes/finance/stock/selectedStock.jsx
@@ -3,3 +3,19 @@ import { useState, useEffect, createContext, useContext } from 'react';
 import { LoadingSpinner } from "../../../components/shared";
 import { getSelectedStock } from "../../../js/finance/stock/stockApiHandler";
 import placeholder from "../../../assets/img/stock/placeholder_chart.jpg"
+
+const SelectedStock = () => {
+    const stocksLoader = async () => {
+        const stock = await getSelectedStock(symbol);
+        if (!stock) {
+            throw new Response("",
+                {
+                    status: 404,
+                    statusText: "Not Found",
+                });
+        }
+        setStock(stock);
+        setLoading(false);
+    }
+}
+export default SelectedStock;
