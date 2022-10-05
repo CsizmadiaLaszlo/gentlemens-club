863a65b79513134be132f64c3d73a81c5e1ea518
Peti <peter.hathazi99@gmail.com>
2022-10-05 21:42:44 +0200

    Update home.js

---
+++ GentlemensClub-Frontend/src/routes/restaurant/home.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+function Home() {
+
+    return (
+        <div>
+            Homepage
+        </div>
+    ); 
+
+}
+
+export default function RestaurantHomeApp() {
+    return <Home /> ;
+}
