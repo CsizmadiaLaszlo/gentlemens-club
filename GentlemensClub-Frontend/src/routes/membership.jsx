5a5241c5ae308b16aa5954387932cba2633142c7
LGyorgy <l.gyorgy90@gmail.com>
2022-10-06 14:43:54 +0200

    Refactor membership page into component file

--- GentlemensClub-Frontend/src/routes/membership.jsx
+++ GentlemensClub-Frontend/src/routes/membership.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import { Button, Card, CardGroup } from "react-bootstrap";
+import MemberShipPage from "../components/membership/membershipPage";
+
 import UserContext from "../services/authentication/userContext";
 
 const Membership = () => {
@@ -19,74 +20,5 @@ const Membership = () => {
         <MemberShipPage />
     );
 }
-
-const MemberShipPage = () => {
-    const cardClasses = "bg-dark text-light border-secondary p-2";
-
-    const cards = [
-        {
-            title: "Bronze Membership",
-            price: "$9.99 / month",
-            features: [
-                "Awesome feature",
-                "Another awesome feature",
-            ]
-        },
-        {
-            title: "Silver Membership",
-            price: "$99.90 / month",
-            features: [
-                "Awesome feature",
-                "Another awesome feature",
-                "Cool feature"
-            ]
-        },
-        {
-            title: "Gold Membership",
-            price: "$999.00 / month",
-            features: [
-                "Awesome feature",
-                "Another awesome feature",
-                "Cool feature",
-                "Best feature"
-            ]
-        },
-    ]
-
-    return (
-        <>
-            <CardGroup>
-                {cards.map(({ title, price, features }) => {
-                    return (
-                        <>
-                            <Card border="light" bg="dark">
-                                <Card.Body>
-                                    <Card.Title>{title}</Card.Title>
-                                    <Card.Text>
-                                        <p>{price}</p>
-                                        <div className="text-center">
-                                            <Button variant="primary">Subscribe now</Button>
-                                        </div>
-                                        <hr></hr>
-                                        <ul className="list">
-                                            {features.map((feature) => {
-                                                return (
-                                                    <>
-                                                        <li>{feature}</li>
-                                                    </>
-                                                );
-                                            })}
-                                        </ul>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </>
-                    );
-                })
-                }
-            </CardGroup>
-        </>
-    );
-};
 
 export default Membership;
