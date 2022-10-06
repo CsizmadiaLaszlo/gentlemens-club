c1c4605decf992ddbbae778897d4dbcd82706ca8
Peti <peter.hathazi99@gmail.com>
2022-10-06 19:13:02 +0200

    Update MenuItem.cs

--- GentlemensClub-Backend/Models/Restaurant/Menu/MenuItem.cs
+++ GentlemensClub-Backend/Models/Restaurant/Menu/MenuItem.cs
@@ -1,13 +1,16 @@
-﻿namespace GentlemensClub.Models.Restaurant.Menu;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GentlemensClub.Models.Restaurant.Menu;
 
 public class MenuItem
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Name { get; set; }
     public string Image { get; set; }
     public List<string> Ingredients { get; set; }
-    public List<SpecialFoodCategories>? SpecialCategories { get; set; }
-    public MenuItemCategory Category { get; set; }
-    public MenuItemSubCategory SubCategory { get; set; }
+    public List<SpecialFoodCategory>? SpecialCategories { get; set; }
+    public MenuItemCategory? Category { get; set; }
+    public MenuItemSubCategory? SubCategory { get; set; }
     public bool isChefFavorite { get; set; }
 }
