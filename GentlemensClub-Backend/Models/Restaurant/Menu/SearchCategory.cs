467b520cdba5f206b81588ef3c8b705a5abd177e
Peti <peter.hathazi99@gmail.com>
2022-10-06 19:13:16 +0200

    Update SearchCategory.cs

--- GentlemensClub-Backend/Models/Restaurant/Menu/SearchCategory.cs
+++ GentlemensClub-Backend/Models/Restaurant/Menu/SearchCategory.cs
@@ -3,5 +3,5 @@
 public class SearchCategoryModel
 {
     public string Name { get; set; }
-    public List<SpecialFoodCategories> Categories { get; set; } = new List<SpecialFoodCategories>();
+    public List<SpecialFoodCategory> Categories { get; set; } = new List<SpecialFoodCategory>();
 }
