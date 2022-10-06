4f0fc5f21f53e119683f69a5e1e99f8ba2728f67
Peti <peter.hathazi99@gmail.com>
2022-10-06 12:39:30 +0200

    Update MenuItemModel.cs

--- GentlemensClub-Backend/Models/Restaurant/Menu/MenuItemModel.cs
+++ GentlemensClub-Backend/Models/Restaurant/Menu/MenuItemModel.cs
@@ -4,6 +4,7 @@ public class MenuItemModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Image { get; set; }
     public List<string> Ingredients { get; set; }
     public List<SpecialFoodCategories>? SpecialCategories { get; set; }
     public bool isChefFavorite { get; set; }
