d405d2504d17b2c09b1e0ce3398e0a238b843962
Csizmadia László <csizilaci92@gmail.com>
2022-10-06 16:24:58 +0200

    Update BankAccountApiController.cs

--- GentlemensClub-Backend/Controllers/ApiControllers/BankAccountApiController.cs
+++ GentlemensClub-Backend/Controllers/ApiControllers/BankAccountApiController.cs
@@ -8,7 +8,7 @@ namespace GentlemensClub.Controllers.ApiControllers;
 
 [Route("api/finance/bank")]
 [ApiController]
-// [Authorize]
+[Authorize]
 public class BankAccountApiController : ControllerBase
 {
     private IBankService BankService { get; set; }
@@ -22,8 +22,8 @@ public class BankAccountApiController : ControllerBase
     [Route("currency")]
     public string GetCurrency([FromQuery] string acronym)
     {
-        // int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
-        var currency = BankService.GetBankCurrency(1, acronym);
+        int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
+        var currency = BankService.GetBankCurrency(userId, acronym);
         return JsonSerializer.Serialize(currency.Result);
     }
     
@@ -31,8 +31,8 @@ public class BankAccountApiController : ControllerBase
     [Route("currencies")]
     public string GetAllCurrency()
     {
-        // int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
-        var currency = BankService.GetAllBankCurrencyByBankAccount(1);
+        int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
+        var currency = BankService.GetAllBankCurrencyByBankAccount(userId);
         return JsonSerializer.Serialize(currency.Result.ToList());
     }
     
@@ -40,8 +40,8 @@ public class BankAccountApiController : ControllerBase
     [Route("transaction")]
     public string GetAllTransaction()
     {
-        // int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
-        var transactions = BankService.GetAllBankTransactionByBankAccount(1);
+        int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
+        var transactions = BankService.GetAllBankTransactionByBankAccount(userId);
         return JsonSerializer.Serialize(transactions.Result.ToList());
     }
     
@@ -49,7 +49,7 @@ public class BankAccountApiController : ControllerBase
     [Route("exchange")]
     public async Task ExchangeCurrency([FromBody] ExchangeDto exchangeData)
     {
-        // int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
-        await BankService.SaveExchange(1, exchangeData);
+        int.TryParse(User.Claims.First(claim => claim.Type == "UserId").Value, out var userId);
+        await BankService.SaveExchange(userId, exchangeData);
     }
 }
