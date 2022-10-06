0ad5354420612213dc6facf484c6b097ce83acd4
Bárdos István <bardos.istvan@icloud.com>
2022-10-06 13:57:00 +0200

    Update Google maps renderer to have customisable width and height properties

--- GentlemensClub-Frontend/src/components/shared.jsx
+++ GentlemensClub-Frontend/src/components/shared.jsx
@@ -17,13 +17,13 @@
     );
 }
 
-export const GoogleMap = (address) => {
+export const GoogleMap = (address, width = 400, height = 300) => {
     const src = `https://maps.google.com/maps?q=${address}&t=&z=17&ie=UTF8&iwloc=&output=embed`
     return (
 
         <div className={"mapouter"} style={{textAlign: "center", alignContent: "center"}}>
             <div className={"gmap_canvas"}>
-                <iframe width={"400"} height={"300"}
+                <iframe width={width} height={height}
                         src={src}>
                 </iframe>
             </div>
