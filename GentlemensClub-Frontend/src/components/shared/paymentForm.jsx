8829e7b883e6eaee329be19bc93932336a6754d7
LGyorgy <l.gyorgy90@gmail.com>
2022-10-20 14:09:46 +0200

    Fix code typo

--- GentlemensClub-Frontend/src/components/shared/paymentForm.jsx
+++ GentlemensClub-Frontend/src/components/shared/paymentForm.jsx
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form';
 
 const PaymentForm = ({onSubmit}) => {
     const handleSubmit = (e) => {
-        e.preventDefault()();
+        e.preventDefault();
         onSubmit(e);
     }
 
@@ -11,13 +11,13 @@ const PaymentForm = ({onSubmit}) => {
         <>
             <Form onSubmit={handleSubmit}>
                 <Form.Label>Name on Card</Form.Label>
-                <Form.Control type="text" placeholder="John Doe" />
+                <Form.Control type="text" required placeholder="John Doe" />
                 <Form.Label>Card Number</Form.Label>
-                <Form.Control type="text" placeholder="1234-5678-9876-5432" />
+                <Form.Control type="text" required placeholder="1234-5678-9876-5432" />
                 <Form.Label>Expiry date</Form.Label>
-                <Form.Control type="text" placeholder="MM/YY" />
+                <Form.Control type="text" required placeholder="MM/YY" />
                 <Form.Label>Security code</Form.Label>
-                <Form.Control type="text" pattern="[0-9]*" maxLength={3} placeholder="CVV" />
+                <Form.Control type="text" required pattern="[0-9]*" maxLength={3} placeholder="CVV" />
                 <div className="my-3 d-grid">
                     <Button className={"btn-block"} variant="primary" type="submit">
                         Subscribe
