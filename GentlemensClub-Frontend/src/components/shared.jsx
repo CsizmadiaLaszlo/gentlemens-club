275b18f8c34567d9760f7f4164cc9e6d756b97ca
Bárdos István <bardos.istvan@icloud.com>
2022-10-21 01:13:07 +0200

    Refactor embedded GoogleMaps component shared styling

--- GentlemensClub-Frontend/src/components/shared.jsx
+++ GentlemensClub-Frontend/src/components/shared.jsx
@@ -21,7 +21,7 @@ export const GoogleMap = (address, width = 400, height = 300) => {
     const src = `https://maps.google.com/maps?q=${address}&t=&z=17&ie=UTF8&iwloc=&output=embed`
     return (
 
-        <div className={"mapouter"} style={{textAlign: "center", alignContent: "center"}}>
+        <div className={"mapouter"}>
             <div className={"gmap_canvas"}>
                 <iframe width={width} height={height}
                         src={src}>
