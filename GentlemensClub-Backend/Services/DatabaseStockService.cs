aa94725abc2bcfb8978bec6db1451ea15ba027cd
szanil <szaniszlolaszlo95@gmail.com>
2022-11-04 09:02:13 +0100

    Create DatabaseStockService.cs

---
+++ GentlemensClub-Backend/Services/DatabaseStockService.cs
@@ -0,0 +1,17 @@
+﻿using GentlemensClub.Data;
+using GentlemensClub.Models.Finance.Bank;
+using GentlemensClub.Services.Interfaces.Finance.Stock;
+using Microsoft.EntityFrameworkCore;
+
+namespace GentlemensClub.Services;
+
+public class DatabaseStockService : IStockDatabaseService
+{
+    private readonly GentlemensClubContext _context;
+
+    public DatabaseStockService(GentlemensClubContext context)
+    {
+        _context = context;
+    }
+}
+
