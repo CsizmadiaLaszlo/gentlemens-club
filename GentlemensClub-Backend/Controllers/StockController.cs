4d6c97f15181f30f028135b16f81a4e58eefa003
szanil <szaniszlolaszlo95@gmail.com>
2022-11-04 09:05:40 +0100

    Create StockController.cs

---
+++ GentlemensClub-Backend/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using GentlemensClub.Models.Stocks;
+using GentlemensClub.Services.Interfaces.Finance.Stock;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GentlemensClub.Controllers;
+
+[Route("api/finance/stockData")]
+[ApiController]
+[Authorize]
+public class StockController : Controller
+{
+    public IStockDatabaseService StockHandler { get; set; }
+
+    public StockController(IStockDatabaseService stockData)
+    {
+        StockHandler = stockData;
+    }
+
+}
+
